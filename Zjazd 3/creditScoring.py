c1c7a9dfcb2f757f4329e10a250e90f6b029b2a4
Pawel Szyszkowski <pawel.szysz@gmail.com>
2020-11-24 01:20:52 +0100

    Prace nad regulami

--- Zjazd 3/creditScoring.py
+++ Zjazd 3/creditScoring.py
@@ -5,9 +5,9 @@ from skfuzzy import control as ctrl
 # New Antecedent/Consequent objects hold universe variables and membership
 # functions
 income = ctrl.Antecedent(np.arange(0, 20001, 100), 'income')
-expenses = ctrl.Antecedent(np.arange(0, 20001, 100) , 'expenses')
-bik_scoring = ctrl.Antecedent(np.arange(0, 11, 1), 'bik_scoring')
-dependents = ctrl.Antecedent(np.arange(0, 9, 1), 'dependents')
+expenses = ctrl.Antecedent(np.arange(0, 20001, 100)  , 'expenses')
+bik_scoring = ctrl.Antecedent(np.arange(0, 11, 1) , 'bik_scoring')
+dependents = ctrl.Antecedent(np.arange(0, 9, 1) , 'dependents')
 scoring = ctrl.Consequent(np.arange(0, 250000, 1000), 'scoring')
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
@@ -23,18 +23,18 @@ scoring['medium'] = fuzz.trimf(scoring.universe, [0, 120000, 250000])
 scoring['high'] = fuzz.trimf(scoring.universe, [120000, 250000, 250000])
 
 # You can see how these look with .view()
-income['average'].view()
+expenses['average'].view()
 
 rule1 = ctrl.Rule(income['poor'] & expenses['good'] & bik_scoring['good'] & dependents['good'], scoring['low'])
-# rule2 = ctrl.Rule(income['average'] | expenses['average'], scoring['medium'])
+rule2 = ctrl.Rule(income['average'] | expenses['average'], scoring['medium'])
 rule3 = ctrl.Rule(income['good'] & expenses['poor'] & bik_scoring['poor'], scoring['high'])
 
 rule1.view()
 
-amount_ctrl = ctrl.ControlSystem([rule1,rule3])
+amount_ctrl = ctrl.ControlSystem([rule1,rule2,rule3])
 amount = ctrl.ControlSystemSimulation(amount_ctrl)
 amount.input['income'] = 20000
-amount.input['expenses'] = 0
+amount.input['expenses'] = 19000
 amount.input['bik_scoring'] = 0
 amount.input['dependents'] = 0
 
